6b3f9dc99b8e78712c13587d772756ebeaf008ad
Hamidreza <hamidreza.tahmooresi@gmail.com>
2024-01-06 15:25:30 +0330

    root module and run other modules

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"mod1/mod1pack1"
+	"mod2/mod2pack1"
 )
 
 func main() {
+	fmt.Println("Module index -> Pacage Main ->  main()")
 	mod1pack1.M1p1app()
+	mod2pack1.M2p1app()
 }
