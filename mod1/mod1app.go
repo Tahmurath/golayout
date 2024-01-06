a7d8b4fc6046162f51cddb4f60797af0f981c0b9
Hamidreza <hamidreza.tahmooresi@gmail.com>
2024-01-06 14:04:43 +0330

    Module 1 & package one

---
+++ mod1/mod1app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"mod1/mod1pack1"
+)
+
+func main() {
+	fmt.Println("Module 1 -> Pacage Main ->  main()")
+	mod1pack1.M1p1app()
+}
---
+++ mod1/mod1pack1/mod1pack1app.go
@@ -0,0 +1,7 @@
+package mod1pack1
+
+import "fmt"
+
+func M1p1app() {
+	fmt.Println("Module 1 -> Pacage One ->  M1p1app()")
+}
