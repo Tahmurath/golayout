7288c00d3236b1a497466e52af6e9f4b94cb7c8b
Hamidreza <hamidreza.tahmooresi@gmail.com>
2024-01-06 14:34:35 +0330

    add module 2 + package one

---
+++ mod2/mod2app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"mod2/mod2pack1"
+)
+
+func main() {
+	fmt.Println("Module 2 -> Pacage Main ->  main()")
+	mod2pack1.M2p1app()
+}
---
+++ mod2/mod2pack1/mod2pack1app.go
@@ -0,0 +1,7 @@
+package mod2pack1
+
+import "fmt"
+
+func M2p1app() {
+	fmt.Println("Module 2 -> Pacage One ->  M2p1app()")
+}
