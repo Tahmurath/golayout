4a05a87336773a21d74b18c43edc9024e85cf915
Hamidreza <hamidreza.tahmooresi@gmail.com>
2024-01-07 10:02:22 +0330

    call module in another module

--- mod1/mod1app.go
+++ mod1/mod1app.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"mod1/mod1pack1"
+	"mod2/mod2pack1"
 )
 
 func main() {
 	fmt.Println("Module 1 -> Pacage Main ->  main()")
 	mod1pack1.M1p1app()
+	mod2pack1.M2p1app()
 }
--- mod2/mod2app.go
+++ mod2/mod2app.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"mod1/mod1pack1"
 	"mod2/mod2pack1"
 )
 
 func main() {
 	fmt.Println("Module 2 -> Pacage Main ->  main()")
 	mod2pack1.M2p1app()
+	mod1pack1.M1p1app()
 }
